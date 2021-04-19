6dafc12599357aabeb7d81ef1a079d7b7f4fd313
Victor Tran <vicr12345@gmail.com>
2021-04-20 00:59:49 +1000

    Implement ordinal i18n

--- racecs-server-frontend/src/common.js
+++ racecs-server-frontend/src/common.js
@@ -1,14 +1,8 @@
+import numeral from 'numeral';
+
 class Common {
     static getOrdinal(number) {
-        let ordinal = "th";
-        if (number % 10 == 1 && number % 100 != 11) {
-            ordinal = "st";
-        } else if (number % 10 == 2 && number % 100 != 12) {
-            ordinal = "nd";
-        } else if (number % 10 == 3 && number % 100 != 13) {
-            ordinal = "rd";
-        }
-        return number + ordinal;
+        return numeral(number).format("0o");
     }
 }
 
--- racecs-server-frontend/src/i18n.js
+++ racecs-server-frontend/src/i18n.js
@@ -2,6 +2,8 @@ import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 import detector from "i18next-browser-languagedetector";
 import backend from "i18next-xhr-backend";
+import numeral from "numeral";
+import "numeral/locales";
 
 let options = {
     interpolation: {
@@ -14,5 +16,12 @@ let setLng = localStorage.getItem("locale");
 if (setLng && setLng !== "system") options.lng = setLng;
 
 i18n.use(detector).use(backend).use(initReactI18next).init(options);
+
+i18n.on("initialized", (options) => {
+    numeral.locale(options.lng.toLowerCase().replace("_", "-"));
+});
+i18n.on("languageChanged", (lng) => {
+    numeral.locale(lng.toLowerCase().replace("_", "-"));
+});
 
 export default i18n;
