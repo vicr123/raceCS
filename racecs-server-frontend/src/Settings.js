8f05134d62b768e787a90898e929835de13521d5
Victor Tran <vicr12345@gmail.com>
2021-04-20 12:34:01 +1000

    Add Dutch setting

--- racecs-server-frontend/src/Settings.js
+++ racecs-server-frontend/src/Settings.js
@@ -122,6 +122,7 @@ class Settings extends React.Component {
             <option value="sys">{this.props.t("SETTINGS_LOCALE_SYSTEM")}</option>
             <option value="en">English</option>
             <option value="de">Deutsch</option>
+            <option value="nl">Nederlands</option>
             <option value="pt_BR">português</option>
             <option value="vi">Tiếng Việt</option>
         </select>
@@ -141,6 +142,7 @@ class Settings extends React.Component {
             <option value="sys">{this.props.t("SETTINGS_LOCALE_SYSTEM")}</option>
             <option value="en">English</option>
             <option value="de">Deutsch</option>
+            <option value="nl">Nederlands</option>
             <option value="pt_BR">português</option>
             <option value="vi">Tiếng Việt</option>
         </select>
